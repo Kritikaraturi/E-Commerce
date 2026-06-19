2f8fda4f7436cecd7242ff5089ea9870706a2313
Kritika Raturi <your_email@gmail.com>
2026-06-19 17:46:32 +0530

    Track database file

--- ecom_project/settings.py
+++ ecom_project/settings.py
@@ -112,15 +112,23 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 
 LOGIN_URL = 'login'
@@ -144,7 +152,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/home/kritikaraturi/static/' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
